d0ca76124dc5d333dc89fef8d11d801de7c7e219
Sandro Torres <sandrotorres.ti@gmail.com>
2020-01-13 10:28:09 -0300

    Primeiros componentes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 
 import Simples from './componentes/Simples'
+import ParImpar from './componentes/ParImpar'
 
 export default class App extends React.Component {
   render() {
@@ -9,6 +10,7 @@ export default class App extends React.Component {
       <View style={styles.container}>
         <Text style={styles.f35}>Meu Primeiro App</Text>
         <Simples texto='Flexível!!!'></Simples>
+        <ParImpar numero={47}></ParImpar>
       </View>
     )
   }
--- src/componentes/Simples.js
+++ src/componentes/Simples.js
@@ -1,9 +1,9 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from 'react';
+import {Text} from 'react-native';
+import Padrao from '../styles/Padrao';
 
-// export default function(props) {    
+// export default function(props) {
 //     return <Text>{props.texto}</Text>
 // }
 
-export default props => <Text>Arrow: {props.texto}</Text>
-
+export default props => <Text style={Padrao.ex}>Arrow: {props.texto}</Text>;
